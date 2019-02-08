0b66c6b4b7028f8b46ed0dc8d1f49ef43c27929e
cameron-bret <c.bret@whiskerhead.com>
2019-02-08 09:28:15 -0500

    How on the Trail of Carmen

--- carmenSolution.sql
+++ carmenSolution.sql
@@ -1,1 +1,18 @@
 SELECT name FROM country WHERE region = 'Southern Europe' ORDER BY population ASC LIMIT 1;
+
+--Italy
+
+SELECT language FROM countrylanguage WHERE countrycode = 'VAT';
+
+--Italian
+
+SELECT * FROM countrylanguage WHERE language = 'Italian' ORDER BY percentage DESC;
+SELECT name FROM country WHERE code = 'SMR';
+
+--San Marino
+
+SELECT * FROM city WHERE name LIKE 'Serr%';
+
+--Serra
+--Serraville
+
